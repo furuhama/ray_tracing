297ad687acfa64aba55425761cc6218bcec34f38
Yusuke Furuhama <furuhama@users.noreply.github.com>
2025-04-27 22:44:05 +0900

    Support anti-aliasing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+mod camera;
 mod image;
 mod ray;
 mod types;
 mod vec3;
+
+use rand::prelude::*;
 
 mod material {
     pub mod dielectric;
@@ -44,20 +47,13 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let samples_per_pixel = 100; // アンチエイリアシング用のサンプル数
 
     // レンダリングの設定
     let max_depth = 50; // 反射の最大回数
 
-    // カメラの基本設定
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal * 0.5 - vertical * 0.5 - Vec3::new(0.0, 0.0, focal_length);
+    // カメラの設定
+    let camera = camera::Camera::new(aspect_ratio);
 
     // シーンの作成
     let mut world = HittableList::new();
@@ -101,12 +97,20 @@ fn main() {
     for j in (0..image_height).rev() {
         println!("Remaining scanlines: {}", j);
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            let mut rng = rand::thread_rng();
 
-            let direction = lower_left_corner + horizontal * u + vertical * v - origin;
-            let ray = Ray::new(origin, direction);
-            let pixel_color = ray_color(&ray, &world, max_depth);
+            // 各ピクセルに対して複数回サンプリング
+            for _ in 0..samples_per_pixel {
+                let u = (i as f64 + rng.gen_range(0.0..1.0)) / (image_width - 1) as f64;
+                let v = (j as f64 + rng.gen_range(0.0..1.0)) / (image_height - 1) as f64;
+
+                let ray = camera.get_ray(u, v);
+                pixel_color = pixel_color + ray_color(&ray, &world, max_depth);
+            }
+
+            // サンプリング結果の平均を取る
+            pixel_color = pixel_color * (1.0 / samples_per_pixel as f64);
             pixels.push(pixel_color);
         }
     }
